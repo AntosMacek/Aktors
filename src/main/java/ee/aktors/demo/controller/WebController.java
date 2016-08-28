0cd069bba2c6e9748aa5422feb952c7fc60461ca
AntosMacek <mawinbm@gmail.com>
2016-08-28 14:02:06 +0300

    hello world

--- src/main/java/ee/aktors/demo/controller/WebController.java
+++ src/main/java/ee/aktors/demo/controller/WebController.java
@@ -1,13 +1,14 @@
 package ee.aktors.demo.controller;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 public class WebController {
 
     @RequestMapping(value = "/")
-    public ModelAndView index() {
-        return new ModelAndView("index");
+    public ModelAndView index(ModelMap model) {
+        return new ModelAndView("index", model);
     }
 
 }
